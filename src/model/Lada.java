544eaac36e853b8554bd70dd0a58be3632959eb8
Szabó Roland <szrtanul@gmail.com>
2025-10-02 15:12:45 +0200

    yea

--- src/model/Lada.java
+++ src/model/Lada.java
@@ -4,14 +4,18 @@
  */
 package model;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  *
  * @author szabo.roland
  */
 public class Lada {
     String[] ladak;
+    boolean megfejtve;
     int megfejtes;
-    int menet = 0;
+    int menet;
     
     public Lada(){
         this.ladak = new String[]{
@@ -19,6 +23,29 @@ public class Lada {
             "Ezüst;Nem én rejtem a kincset",
             "Bronz;Az arany hazudik",
         };
+        this.megfejtve=false;
         this.megfejtes=1;
+        this.menet=0;
+    }
+    
+    public boolean doValaszt(int ladaszam){
+        if(!this.megfejtve || this.menet < ladak.length){
+            menet++;
+            megfejtve=this.megfejtes==ladaszam;
+        }
+        return false;
+    }
+    
+    String[] getLadak(){
+        return Arrays.copyOf(ladak, ladak.length);
+    }
+    
+    public boolean getMegfejtve(){
+        return this.megfejtve;
+    }
+    
+    public int getMegfejtes(){
+        boolean both = this.megfejtve || this.menet >= ladak.length;
+        return both ? megfejtes : -1;
     }
 }
